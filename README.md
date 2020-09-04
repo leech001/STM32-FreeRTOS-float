f375653d234bb164a996c45ceeb2815ca31044e2
Konstantin Bulanov <leech001@gmail.com>
2020-09-04 15:09:49 +0300

    Update version to June 2020. Add Example code/

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Configuring STM32 to work with float and double (converting values ​​to char type) under FreeRTOS.
+# Configuring STM32 to work with float and double (converting values to char type) under FreeRTOS use printf, sprintf, snprintf.
 
 If you use standard tools for converting float and double to char type such as printf, sprintf, snprintf, then when you run these conversions under FreeRTOS and using the GNU Arm Embedded Toolchain (https://developer.arm.com/tools-and-software/ open-source-software / developer-tools / gnu-toolchain / gnu-rm) the microcontroller hangs and a Hard Fault error occurs.
 According to research on this topic http://www.nadler.com/embedded/newlibAndFreeRTOS.html, this error is associated with incorrect operation of dynamic memory allocation (malloc / malloc_r).
 
+Source code https://github.com/DRNadler/FreeRTOS_helpers
+
 How is it all corrected.
-1. Remove the heap_4.c file from the \ Middlewares \ Third_Party \ FreeRTOS \ Source \ portable \ MemMang \ directory of your project
-2. Copy the heap_useNewlib.c file from the given repository (src) to the above directory;
-3. Open the FreeRTOS configuration file (FreeRTOSConfig.h) and add an additional define to the section;
+1. Remove the ```heap_4.c``` file from the ```\Middlewares\Third_Party\FreeRTOS\Source\portable\MemMang\``` directory of your project
+2. Copy the ```heap_useNewlib_ST.c``` file from the given repository (src) to the above directory(src);
+3. Comment or delete define function ```caddr_t _sbrk(int incr)``` in file ```Core\Src\syscalls.c```
+4. Open the FreeRTOS configuration file (```FreeRTOSConfig.h```) and add an additional define to the section;
 ```
 /* USER CODE BEGIN Defines */   	      
 /* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
@@ -21,7 +24,7 @@ This completes the setup. Compilation and operation of the printf, sprintf, snpr
 
 ### Do not forget that when you re-generate the project in STM32CubeMX, the procedure will need to be repeated!
 
-p.s. The version of the heap_useNewlib.c file provided in this repository is slightly different from the original (http://www.nadler.com/embedded/newlibAndFreeRTOS.html) in terms of fixing compilation errors. It was tested on the following versions of the STM32F103C8T6 and STM32F401CCU6 microcontrollers under Windows 10 and Ubuntu 19.10
+p.s. The version of the ```heap_useNewlib_ST.c``` file provided in this repository is slightly different from the original (https://github.com/DRNadler/FreeRTOS_helpers) in terms of fixing compilation errors. It was tested on the following versions of the STM32F401CCU6 microcontrollers under Windows 10 and Ubuntu 20.04
 
 
 # Настройка STM32 для работы с float и double (преобразования значений к типу char) под FreeRTOS.
@@ -31,9 +34,10 @@ p.s. The version of the heap_useNewlib.c file provided in this repository is sli
 Согласно исследованиям этой темы http://www.nadler.com/embedded/newlibAndFreeRTOS.html данная ошибка связанна с некорректной работой динамического выделения памяти (malloc/malloc_r).
 
 Как это все правиться:
-1. Удалите из директори \Middlewares\Third_Party\FreeRTOS\Source\portable\MemMang\ вашего проекта файл heap_4.c;
-2. Скопируйте в вышеуказанную директорию файл heap_useNewlib.c из данного репозитария (src);
-3. Откройте конфигурационный файл FreeRTOS (FreeRTOSConfig.h) и добавьте в секцию дополнительный define;
+1. Удалите из директори `````\Middlewares\Third_Party\FreeRTOS\Source\portable\MemMang\``` вашего проекта файл ```heap_4.c```;
+2. Скопируйте в вышеуказанную директорию файл ```heap_useNewlib.c``` из данного репозитария (src);
+3. Закоментируйте или удалите функцию ```caddr_t _sbrk(int incr)``` в файле ```Core\Src\syscalls.c```
+4. Откройте конфигурационный файл FreeRTOS (FreeRTOSConfig.h) и добавьте в секцию дополнительный define;
 ```
 /* USER CODE BEGIN Defines */   	      
 /* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
@@ -49,4 +53,4 @@ p.s. The version of the heap_useNewlib.c file provided in this repository is sli
 
 ### Не забывайте, что при пере генерации проекта в STM32CubeMX процедуру необходимо будет повторить!
 
-p.s. Версия файла  heap_useNewlib.c приведенного в данном репозитарии немного отличается от исходной (http://www.nadler.com/embedded/newlibAndFreeRTOS.html) в части исправления ошибок компиляции. Проверялось на следующих версиях микроконтроллеров STM32F103C8T6 и STM32F401CCU6 под Windows 10 и Ubuntu 19.10
+p.s. Версия файла  ```heap_useNewlib.c``` приведенного в данном репозитарии немного отличается от исходной (https://github.com/DRNadler/FreeRTOS_helpers) в части исправления ошибок компиляции. Проверялось на следующих версиях микроконтроллеров STM32F401CCU6 под Windows 10 и Ubuntu 20.04
---
+++ example/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/bulanov/Project/FreeRTOS/startup/startup_stm32f401xc.s" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/startup/startup_stm32f401xc.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F401xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bulanov/Project/FreeRTOS/Core/Src/freertos.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/freertos.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/gpio.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/gpio.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/main.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/main.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/stm32f4xx_hal_msp.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/stm32f4xx_hal_timebase_tim.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/stm32f4xx_it.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/syscalls.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/syscalls.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Core/Src/system_stm32f4xx.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_useNewlib_ST.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_useNewlib_ST.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/home/bulanov/Project/FreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/bulanov/Project/FreeRTOS/cmake-build-debug/CMakeFiles/FreeRTOS.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F401xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
